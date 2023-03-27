c0cf86c36819ed3447b18cbe1e6b51689da221f2
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2023-03-26 22:26:57 -0700

    new

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 let links = document.getElementsByTagName("a");
 
-
+// because smaller than --links.length-- , the last link is not printed
 for (let i = 0; i < links.length; i++) {
     console.log("this is link number " + i);
     
 }
+
+
+// *If you want the last link to be printed, you must:
+// for (let i = 0; i <= links.length; i++) {
+//     console.log("this is link number " + i);
+// }
 
 document.write("All links now looped");
